c1fce1cc1518be00b867aae509128f6892def908
waichou <1935235867@qq.com>
2018-05-24 14:01:19 +0800

    native:commit code

--- app/src/main/java/com/test/greendao/greendaoproject/MainActivity.java
+++ app/src/main/java/com/test/greendao/greendaoproject/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("这个是源文件");
     }
 }
